62db8731bc66bcef9b04bee296840221e08e3c52
Anastasiya Karpenka <knesty@yandex.by>
2023-09-07 15:02:18 +0300

    added preview of gallery

--- components/previewFile/PreviewFile.js
+++ components/previewFile/PreviewFile.js
@@ -27,6 +27,8 @@ export function previewFileAsImg(file, elementToAppend, handleDelete) {
 
 
 export function previewFilesAsCanvas(file, elementToAppend) {
+    let app = document.querySelector('#app');
+
     let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onloadend = function() {
@@ -35,13 +37,13 @@ export function previewFilesAsCanvas(file, elementToAppend) {
 
         let originalImg = document.createElement('img');
         originalImg.src = reader.result;
-        originalImg.id = file.lastModified;
 
         let newImg = document.createElement('img');
         newImg.src = watermakImageWithText(
             originalImg,
             "some text"
           );
+        newImg.id = file.lastModified; 
 
         div.append(newImg);
 
@@ -52,8 +54,23 @@ export function previewFilesAsCanvas(file, elementToAppend) {
                 <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/>
             </svg>
         `
-        eye.addEventListener('click', ()=> {
-            console.log('see');
+        eye.addEventListener('click', () => {
+            let div = document.createElement('div');
+            div.id = 'bigImgContainer';
+
+            let bigImg = document.createElement('img');
+            bigImg.src = newImg.src;
+            bigImg.id = "bigImg";
+
+            let closeBtn = document.createElement('a');
+            closeBtn.id = 'closeBigImg';
+            closeBtn.innerText = 'x';
+            closeBtn.addEventListener('click', () => {
+                app.removeChild(div);
+            });
+
+            div.append(bigImg, closeBtn);
+            app.appendChild(div);
         });
         div.append(eye);
 
